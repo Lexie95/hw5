112059db048abde14fec6d010be29baecbfcec91
Lexie95 <32326197+Lexie95@users.noreply.github.com>
2017-11-08 00:08:21 +0100

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,85 +1,74 @@
-var song, analyzer;
-
-function draw() {
-  background(0);
-  image(fingers,150,150); // draw a second copy to canvas
-}
+var mySong;
+var myImg;
+var analyzer;
 
 function preload() {
-  song = loadSound("./assets/love.mp3");
+  mySong = loadSound("./assets/KLDYKXQ.mp3");
+  myImg = loadImage("./assets/maxresdefault.jpg");
 }
 
 function setup() {
-  song.loop();
-
-  // create a new Amplitude analyzer
+  createCanvas(windowWidth,windowHeight);
+  
   analyzer = new p5.Amplitude();
-
-  // Patch the input to an volume analyzer
-  analyzer.setInput(song);
+  analyzer.setInput(mySong);
+  mySong.loop();
+  angleMode(DEGREES);
 }
 
 function draw() {
-  background(0);
-
-  // Get the average (root mean square) amplitude
+    // Get the average (root mean square) amplitude
   var rms = analyzer.getLevel();
-  fill(127);
-  stroke(0);
-
-  // Draw an ellipse with size based on volume
-  ellipse(width/2, height/2, 10+rms*200, 10+rms*200);
-}
-
-var colorList = ['#3498db',
-    '#d303dc',
-    '#fbc218',
-    '#25d36d',
-    '#2abcfa',
-    '#031754',
-    '#eff518',
-    '#0596c3',
-];
-
-
-function setup() {
-
-    createCanvas(1000, 800);
-    frameRate(5);
-}
-
-var song, analyzer;
-
-function preload() {
-  song = loadSound('./assets/KLDYKXQ.mp3');
-}
-
-function setup() {
-  createCanvas(1000, 800);
-  song.loop();
-
-  // create a new Amplitude analyzer
-  analyzer = new p5.Amplitude();
-
-  // Patch the input to an volume analyzer
-  analyzer.setInput(song);
-}
-
-function draw() {
   
-  // Get the average (root mean square) amplitude
-  var rms = analyzer.getLevel();
-  fill(127);
-  stroke(0);
   
-for(var x = 0; x < 3000; x += 40) {
-    for(var y = 0; y < 1000; y += 40) {
-    var index = floor(random() * colorList.length);
-    var colorHex = colorList[index];
-     fill(color(colorHex));
-     noStroke();
-      ellipse(x, y, 1+rms*100, 1+rms*100);
-    }
+  
+  var dim= map(vol,0,1,width/10,width/2);
+  push();
+  for(var x=0; x<width; x=x+width/20){
+  strokeWeight(3);
+  stroke(255);
+  line(x,0,x,height);
   }
+  pop();
+  
+  
+  
+  if(mySong.isPlaying() === true) {
+    background(0,0,225);
+    
+    var vol = analyzer.getLevel();
+    image(myImg,width/4,height/4,vol*1000,vol*600);
+    noStroke();
+    fill("#FEB7E2");
+    ellipse(random(0,width),random(0,height),vol*100);
+    fill(0,0,255);
+    ellipse(random(0,width),random(0,height),vol*200);
+    
+    
+    angleMode(DEGREES);
+    noFill();
+    stroke("#FEB7E2");
+    strokeWeight(20);
+    rect(width/4-10,height/4-10,width/2,height/2)
+    push();
+    fill("#FDC920");
+    ellipse(width/4-10,height/4-10,vol*200)
+    pop();
+    strokeWeight(2);
+    rect(random(0,width),random(0,height),vol*300,vol*100)
+  }
+  
+  if(mouseX>=width/2-height/7*2 && mouseX<=width/2+height/7*2 && mouseY>=height/7 && mouseY<=height/7+height/7*4) {
+    textFont('Garamond');
+    textAlign(CENTER);
+    textSize(18);
+    noStroke();
+    fill("#FDC920");
+    text("Croatian Rhapsody",width/2,height/1.5-30);
+    text("Maksim Mrvica",width/2,height/1.5);
+  }
+}
 
+function windowResized() {
+  resizeCanvas(windowWidth,windowHeight);
 }
